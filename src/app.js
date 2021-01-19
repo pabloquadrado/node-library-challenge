4d7905802aad2770e11ccbc5d8a691026f397186
Pablo Quadrado <pabloquadrado27@gmail.com>
2021-01-19 14:09:01 -0300

    Ajusta variável de ambiente que define a porta que irá rodar a aplicação

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(router);
 
 if (process.env.NODE_ENV !== 'test') {
-    app.listen(process.env.APP_PORT || 3333, () => {
+    app.listen(process.env.PORT || 3333, () => {
         console.log('Server is running');
     });
 }
